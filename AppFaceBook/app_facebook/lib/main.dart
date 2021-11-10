ff0deb5064f720448b1ac05136607a10f24041a6
VietTinh <0306191473@caothang.edu.vn>
2021-11-10 14:39:46 +0700

    add_demotab

--- AppFaceBook/app_facebook/lib/main.dart
+++ AppFaceBook/app_facebook/lib/main.dart
@@ -118,18 +118,57 @@ class LoadingScreen extends StatelessWidget {
                       builder: (context) {
                         return AlertDialog();
                       });
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => MyHomePage(),
-                  //   ),
-                  // );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyHomePage(),
+                    ),
+                  );
                 }
               },
             ),
           ),
         ],
       )),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home)),
+                Tab(icon: Icon(Icons.people_alt_outlined)),
+                Tab(icon: Icon(Icons.notification_add_outlined)),
+                Tab(icon: Icon(Icons.menu)),
+              ],
+            ),
+            title: const Text('Home',style: TextStyle(fontFamily:"RobotoMono" ),),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.home),
+              Icon(Icons.people_alt_outlined),
+              Icon(Icons.notification_add_outlined),
+              Icon(Icons.menu),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
