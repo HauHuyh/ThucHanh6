6ebbffba0913c870dc674ab204606115662c2895
VietTinh <0306191473@caothang.edu.vn>
2021-11-10 14:36:48 +0700

    add_fonts_and_login

--- AppFaceBook/app_facebook/lib/main.dart
+++ AppFaceBook/app_facebook/lib/main.dart
@@ -12,104 +12,124 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoadingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+TextEditingController _user = new TextEditingController();
+TextEditingController _password = new TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(
+          'Sign In',
+          style: TextStyle(fontFamily: "RobotoMono"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.cyan,
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 70, bottom: 20),
+            child: SizedBox(
+              height: 120,
+              width: 120,
+              child: Container(
+                  child: FlatButton(
+                onPressed: () {},
+                color: Colors.white,
+                child: Icon(
+                  Icons.lock_outline,
+                  size: 70,
+                  color: Colors.lightBlue.shade200,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+              )),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 50),
+              width: 425,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _user,
+                    decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.people_alt_outlined,
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        hintText: 'User'),
+                  ),
+                  // Checkbox(value: true, onChanged: (value) {}),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    controller: _password,
+                    decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(
+                          Icons.lock_outline,
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'Password'),
+                  ),
+                ],
+              )),
+          Container(
+            width: 425,
+            height: 50,
+            margin: EdgeInsets.only(top: 100),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black, // background
+              ),
+              child: Text('SIGN IN',
+                  style:
+                      TextStyle(color: Colors.white, fontFamily: "RobotoMono")),
+              onPressed: () {
+                if (_user.text == "" && _password.text == "") {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Text(
+                            "Chưa nhập thông tin",
+                            style: TextStyle(fontFamily: "RobotoMono"),
+                          ),
+                        );
+                      });
+                } else if (_user.text == _password.text) {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog();
+                      });
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => MyHomePage(),
+                  //   ),
+                  // );
+                }
+              },
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
